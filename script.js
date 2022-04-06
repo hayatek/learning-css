d2d6e253888cd5c767e19cbb61759841950586ca
hayatek <stream.hyt@gmail.com>
2022-04-06 23:42:18 +0900

    update style.css

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const targetElement = document.querySelectorAll(".animationTarget");
+console.log(targetElement);
+document.addEventListener("scroll", function () {
+  for (let i = 0; i < targetElement.length; i++) {
+    const getElementDistance =
+      targetElement[i].getBoundingClientRect().top +
+      targetElement[i].clientHeight * 0.6;
+    if (window.innerHeight > getElementDistance) {
+      targetElement[i].classList.add("show");
+    }
+  }
+});
