c2d1243f8fc22e73fcee2ec945cafb4078d9457e
hayatek <stream.hyt@gmail.com>
2022-04-09 21:30:15 +0900

    updated script.js

--- textAnimation/script.js
+++ textAnimation/script.js
@@ -1,1 +1,12 @@
-const targetElement = document.querySelectorAll(".animationTarget");
+const AnimationTargetElement = document.querySelectorAll(".textAnimation");
+for (let i = 0; i < AnimationTargetElement.length; i++) {
+  const targteElement = AnimationTargetElement[i];
+  texts = targteElement.textContent;
+  tesxsArray = [];
+  console.log(texts);
+  targteElement.textContent = "";
+  for (let j = 0; j < texts.split("").length; j++) {
+    tesxsArray.push("<span>" + texts.split("")[j] + "</span>");
+  }
+  console.log(tesxsArray);
+}
